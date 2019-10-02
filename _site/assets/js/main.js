be9f13ba70bbfda5466dd07ddb29600e9ca90e06
Joselu <jose.gallardo994@gmail.com>
2019-10-02 20:18:49 +0200

    feature: ALL DELETED II THE REVENGE

--- _site/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-function random_number(first, last) {
-    return Math.floor((Math.random() * (last - first + 1))) + first;
-}
-
-$(document).ready(function(){
-    $('body').bootstrapMaterialDesign();
-});
